bbd83da34819dc3e897bb15a0789cf39bf30964f
ronildo-junior <ronildo_juninho@hotmail.com>
2021-08-24 09:33:12 -0300

    Add custom query findAll on Restaurant to fix N+1 problem.

--- src/main/java/com/ronijr/algafoodapi/domain/repository/RestaurantRepository.java
+++ src/main/java/com/ronijr/algafoodapi/domain/repository/RestaurantRepository.java
@@ -1,6 +1,7 @@
 package com.ronijr.algafoodapi.domain.repository;
 
 import com.ronijr.algafoodapi.domain.model.Restaurant;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -11,4 +12,8 @@ public interface RestaurantRepository
         extends CustomJpaRepository<Restaurant, Long>, RestaurantRepositoryQueries {
     @Override
     List<Restaurant> findCriteriaTest(Map<String, Object> parameters);
+    @Query("from Restaurant r join fetch r.cuisine " +
+            "left join fetch r.address.city " +
+            "left join fetch r.address.city.state")
+    List<Restaurant> findAll();
 }
